8614e2eea34caad8b200a7adfbd8b5bb8285dcea
singhd1 <singhd1@sfasu.edu>
2022-10-09 15:16:21 -0500

    CircleInPane

--- src/App.java
+++ src/App.java
@@ -1,19 +1,32 @@
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.layout.Pane;
 import javafx.scene.layout.StackPane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
 import javafx.stage.Stage;
 
 public class App extends Application { 
     @Override
     public void start(Stage primaryStage) { 
-        StackPane pane = new StackPane(); // Create Parent Object
-        pane.getChildren().add(new Button("OK")); // add node to the pane(Parent)
-        Scene scene = new Scene(pane, 200, 50); // add pane to the scene
-        primaryStage.setTitle("Button in a pane");
+        Circle circle = new Circle();
+        circle.setCenterX(100);
+        circle.setCenterY(100);
+        circle.setRadius(50);
+        circle.setFill(Color.WHITE);
+        circle.setStroke(Color.RED);
+
+        //Pane stackPane = new StackPane(circle);
+        // or
+        Pane stackPane = new StackPane();
+        stackPane.getChildren().add(circle);
+
+        Scene scene = new Scene(stackPane, 200, 200);
+
         primaryStage.setScene(scene);
-        primaryStage.show();   
-        
+
+        primaryStage.show();
     }
 
     public static void main(String[] args) throws Exception {
